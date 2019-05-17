40a288e3e28b8fc73016e0ada1451aab4c4b68b8
camalzade_elvin <camalzade_elvin@outlook.com>
2019-05-17 21:05:46 +0400

    Some classes are ready . . .

---
+++ ReposityWpfApp5/Domain/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReposityWpfApp5.Domain.Entities
+{
+   public class Student
+    {
+        public int Id { get; set; }
+        public int No { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
+
+
